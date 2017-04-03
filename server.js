07cdba67f712f75185dd416bdec4602f23fc75b5
Emil Skovsen Olsen <emil@Emils-MacBook-Pro.local>
2017-04-03 10:21:58 +0200

    Added express server file to serve from dist folder. This fixes bug with urls.

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+const PORT = 3006
+
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/img', express.static(__dirname + '/img'));
+app.use(express.static(__dirname + '/dist'));
+
+
+app.all('/', function(req, res, next) {
+    // Just send the index.html for other files to support HTML5Mode
+    res.sendFile('index.html', { root: __dirname + '/dist' });
+});
+
+console.log('Starting server and listening on port: ' + PORT);
+app.listen(PORT); //the port you want to use
